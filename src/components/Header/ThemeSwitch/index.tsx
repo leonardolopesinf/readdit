edb33fc04b1808ea998153b1059097febcffe0be
Leonardo Lopes <leonardolopesinf@gmail.com>
2021-09-29 03:06:52 -0300

    refact: Transformando input do ThemeSwitch em uncontrolledInput

--- src/components/Header/ThemeSwitch/index.tsx
+++ src/components/Header/ThemeSwitch/index.tsx
@@ -1,18 +1,36 @@
-import React, { useContext, useMemo } from "react";
+import React, { useCallback, useContext, useMemo, useRef } from "react";
 import { ThemeContext } from "../../../context/ThemeContext";
 import { SwitchContainer, SwitchInput } from "./styles";
 
 const ThemeSwitch: React.FC = () => {
   const { themeName, toggleTheme } = useContext(ThemeContext);
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const switchValue = useMemo(() => themeName === "dark", [themeName]);
+
+  const toggleSwitch = useCallback(() => {
+    const { current } = inputRef;
+
+    if (current) {
+      current.value = themeName === "dark" ? "light" : "dark";
+      current.checked = !switchValue;
+
+      toggleTheme();
+    }
+  }, [toggleTheme, themeName, switchValue]);
 
   return (
     <SwitchContainer>
-      <SwitchInput onClick={toggleTheme}>
+      <SwitchInput className="theme-switch" onClick={toggleSwitch}>
         <span className="moon">&#127772;</span>
         <span className="sun">&#127774;</span>
-        <input checked={switchValue} type="checkbox" />
+        <input
+          ref={inputRef}
+          defaultValue={themeName}
+          defaultChecked={switchValue}
+          type="checkbox"
+        />
         <div className="circle" />
       </SwitchInput>
     </SwitchContainer>
