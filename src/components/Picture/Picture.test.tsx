1c86c43d14a264551485887b164e28f79dd0d6da
Leonardo Lopes <leonardolopesinf@gmail.com>
2021-10-01 04:10:52 -0300

    test: Criando funcao para montar o componente de Picture nos testes

--- src/components/Picture/Picture.test.tsx
+++ src/components/Picture/Picture.test.tsx
@@ -1,13 +1,14 @@
 import { mount } from "@cypress/react";
 import Picture from ".";
 
+const mountPicture = (src: string) => mount(<Picture src={src} alt="img" />);
+
 describe("Skeleton", () => {
-  it("should render src if it loads ", () => {
+  it("should render src if it loads", () => {
     const imageSrc =
       "https://lh3.googleusercontent.com/proxy/pD4CF7jHfC4f3T1qeBWmd3n9JE0r_x1VI2AMvrqHIBJszzTnbay9b-jna_ctJrgZf6TRq6hOr_pVAt6Fxd_zUQC6TnA";
 
-    mount(<Picture src={imageSrc} alt="img" />);
-
+    mountPicture(imageSrc);
     cy.wait(500);
 
     return cy.get("[data-testid=picture]").then(($picture) => {
@@ -17,13 +18,12 @@ describe("Skeleton", () => {
     });
   });
 
-  it("should render default image if src not load ", () => {
+  it("should render default image if src not load", () => {
     const imageSrc = "";
     const defaultImageSrc =
       "https://cdn-icons-png.flaticon.com/512/52/52053.png";
 
-    mount(<Picture src={imageSrc} alt="img" />);
-
+    mountPicture(imageSrc);
     cy.wait(500);
 
     return cy.get("[data-testid=picture]").then(($picture) => {
